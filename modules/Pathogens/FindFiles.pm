59a22ba2eb111b5fad6338fe4405779fb94e3f39
andrewjpage@gmail.com <ap13@sanger.ac.uk>
2011-07-28 10:20:12 +0100

    exclude directories and cache uid to usernames

--- modules/Pathogens/FindFiles.pm
+++ modules/Pathogens/FindFiles.pm
@@ -26,27 +26,28 @@ has 'directory'           => ( is => 'rw', isa => 'Str', required   => 1 );
 has 'exclude'             => ( is => 'rw', isa => 'Maybe[Str]' );
 has 'regex'               => ( is => 'rw', isa => 'Str', required   => 1 );
 has '_output_file_handle' => ( is => 'rw' );
+has '_uid_to_username'    => ( is => 'rw', isa => 'HashRef', default => sub { {} } );
 
 sub BUILD
 {
   my $self = shift;
   open(my $ofh, '+>', $self->output_file) or die "Couldnt open output file: ".$self->output_file." $!";
   $self->_output_file_handle($ofh);
+  my $exclude_regex = $self->exclude;
   
   find(
-    sub {
-      _wanted({self => $self});
-      _preprocess({self => $self});
+    { 
+      wanted => sub {
+        _wanted({self => $self});
+      },
+      preprocess => sub {
+        grep { $_ !~ /$exclude_regex/ } @_; 
+      }
     },
-     $self->directory
-    );
+    $self->directory
+  );
 
   close($self->_output_file_handle);
-}
-
-sub _preprocess{
-  my $self = ${$_[0]}{self};
-  grep { $_ !~ /$self->exclude/ } @_;
 }
 
 sub _wanted {
@@ -62,13 +63,28 @@ sub _wanted {
   }
 }
 
+sub _find_username_from_uid
+{
+  my $self = shift;
+  my $uid = shift;
+  
+  my $username = %{$self->_uid_to_username}->{$uid};
+  unless(defined $username)
+  {
+    $username = (getpwuid($uid))[0];
+    %{$self->_uid_to_username}->{$uid} = $username;
+  }
+
+  return $username
+}
+
 sub _find_file_details
 {
   my $self = shift;
   my $filename = shift;
   my ($uid, $size) = (lstat($filename))[4,7];
   return unless(defined $uid);
-  my ($username) = (getpwuid($uid))[0];
+  my $username = $self->_find_username_from_uid($uid);
   $username = $uid unless defined $username;
   my @user_details = ($username, $size);
   return @user_details;
